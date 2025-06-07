ca7eafbf640d529e517046caa5c89aebb3bd05ce
Gavin Meeker <gmeeke99@gmail.com>
2025-06-07 14:57:01 -0400

    add title and monthly income input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { InputNumber, Typography, Flex } from 'antd';
 
+const { Title, Text} = Typography;
 function App() {
     return (
         <>
-            <div>hello</div>
-            <div>hello</div>
+            <Title>Create a budget</Title>
+            <div>
+                <Flex vertical gap={8}>
+                    <Text>Monthly Income</Text>
+                    <InputNumber<number>
+                        style={{backgroundColor: "#EBF0ED", color: "#618570"}}
+                        width="150"
+                        prefix="$"
+                        formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                        parser={(value) => value?.replace(/\$\s?|(,*)/g, '') as unknown as number}
+                    />
+                </Flex>
+
+            </div>
         </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {ConfigProvider} from "antd";
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <ConfigProvider theme={{
+            token: {
+                colorPrimary: "4AA36B"
+            },
+            components: {
+                InputNumber: {
+                    controlWidth: 150
+            }
+        }}}>
+            <App/>
+        </ConfigProvider>
+    </StrictMode>,
 )
