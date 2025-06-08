0ffe742659d9d102833e6ee7a9618a40a961e310
Gavin Meeker <gmeeke99@gmail.com>
2025-06-08 18:02:27 -0400

    replace ant ui with material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import { InputNumber, Typography, Flex } from 'antd';
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
-const { Title, Text} = Typography;
-function App() {
+const App = () => {
     return (
         <>
-            <Title>Create a budget</Title>
-            <div>
-                <Flex vertical gap={8}>
-                    <Text>Monthly Income</Text>
-                    <InputNumber<number>
-                        style={{backgroundColor: "#EBF0ED", color: "#618570"}}
-                        width="150"
-                        prefix="$"
-                        formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                        parser={(value) => value?.replace(/\$\s?|(,*)/g, '') as unknown as number}
-                    />
-                </Flex>
-
-            </div>
+            <Typography>Hello</Typography>
+            <Button variant="contained">Hello</Button>
         </>
     )
 }
